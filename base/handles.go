3b5ee2ab6d0deed5fce6201cf70a88cf4ad074b1
Zane Finner <zanefinnergm@gmail.com>
2020-05-20 18:20:43 -0400

    introducing templates

--- base/handles.go
+++ base/handles.go
@@ -1,11 +1,28 @@
 package base
 
 import (
-	"net/http"
 	"fmt"
+	"html/template"
+	"net/http"
 )
+
 //IndexHandle Resolves the site's root
-func IndexHandle(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Website Root"))
+func IndexHandle(w http.ResponseWriter, r *http.Request) {
+	//w.Write([]byte("Website Root"))
+	t, err := template.New("foo").Parse(`<!doctype HTML>
+	<html>
+		<head>
+			<title>Hello World</title>
+		</head>
+		<body>
+			<p>Hello, {{.}}</p>
+		</body>
+	</html>
+		`)
+	err = t.Execute(w, template.HTML(`<b>Zane</b>`))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	fmt.Println("Endpoint Hit: Website Root")
 }
