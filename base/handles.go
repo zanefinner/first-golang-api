20a9faaa8ac0fc0af2c8451728b6777dd76ee807
Zane Finner <zanefinnergm@gmail.com>
2020-05-20 18:53:23 -0400

    modular landing page

--- base/handles.go
+++ base/handles.go
@@ -6,15 +6,34 @@ import (
 	"net/http"
 )
 
+var (
+	t      *template.Template
+	err    error
+	config map[string]string
+)
+
 //IndexHandle Resolves the site's root
 func IndexHandle(w http.ResponseWriter, r *http.Request) {
 	//w.Write([]byte("Website Root"))
-	t, err := template.ParseFiles("templates/landing_page.html")
+	t, err = template.ParseFiles("templates/partials/head.html")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	config := map[string]string{
+	config = map[string]string{
+		"title": "Welcome",
+	}
+	err = t.Execute(w, config)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	t, err = template.ParseFiles("templates/base/landing_page.html")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	config = map[string]string{
 		"name": "Zane",
 	}
 	err = t.Execute(w, config)
@@ -22,5 +41,6 @@ func IndexHandle(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		return
 	}
+
 	fmt.Println("Endpoint Hit: Website Root")
 }
